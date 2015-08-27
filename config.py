7a89bc007342c9ecdde7b74a2e3627c4932cbe24
salus-sage <salus.sage@gmail.com>
2015-08-27 15:30:57 +0530

    Feature enhancement: Audio Swtr - Swts about audio files. With swtstore oauth and sign in. Pipeline: To be able to post sweets from UI - Play area Backbone router and history feature

---
+++ config.py
@@ -0,0 +1,30 @@
+DEBUG = True
+HOST = '127.0.0.1'
+
+# secret key for the application used in session
+
+SECRET_KEY = "aongndom ring dalvnython UID"
+
+# the URL pointing to the sweet store this application will sweet to
+SWTSTORE_URL = 'http://localhost:5001'
+
+
+# the URL at which your application is hosted
+# when you are deploying the app locally, by default this should be
+#app_url= 'http://localhost:5000'
+APP_URL = 'http://localhost:5000'
+
+# the app_id or client_id you have recieved when you registered this
+# application to swtstore
+APP_ID = 'F1Cv7775QToi3wKMDjSIqYwAPpemwlZoqwDcYaSH'
+
+# the app_secret or client_secret you have recieved when you registered this
+# application to swtstore
+APP_SECRET = 'k924KUu8SYhEw9FKoR4yunPxDn6QUtP27TYJnZ267h2cXykHtN'
+
+# the absolute url of the OAuth2.0 redirect endpoint
+# this is the endpoint where the second part of the oauth handshake happen and
+# the endpoint passes the client secret and the recvd code for the final call
+# to recieve the OAuth token. For this app, the endpoint is /authenticate
+REDIRECT_URI = 'http://localhost:5000/authenticate'
+
